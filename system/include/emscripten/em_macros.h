8732d397f5a2eb15352d2ab44dd5918083f89ef4
Sam Clegg <sbc@chromium.org>
2020-02-26 21:50:47 -0800

    Experiment with `import_name` and `import_module` attributes
    This should allow us to one day remove the `--allow-undefined` linker
    flag since all JS imports should be tagged in the source code in this
    way.
    
    This change is enough to allow hello world to build with out
    `--allow-undefined`

---
+++ system/include/emscripten/em_macros.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2020 The Emscripten Authors.  All rights reserved.
+ * Emscripten is available under two separate licenses, the MIT license and the
+ * University of Illinois/NCSA Open Source License.  Both these licenses can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#define EM_EXPORT __attribute__((used))
+#define EMSCRIPTEN_KEEPALIVE EM_EXPORT
+
+#ifdef __wasm__
+#define EM_IMPORT(NAME) __attribute__((import_module("env"), import_name(#NAME)))
+#else
+#define EM_IMPORT(NAME)
+#endif
